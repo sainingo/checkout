316f1c5f7ee54fc83ab6d3da9d728ee600283936
sainingo <nickmentor421@gmail.com>
2022-09-01 16:56:56 +0300

    Setup routes

---
+++ back-end/app/controllers/api/v1/category_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::CategoryController < ApplicationController
+    def index
+        @category = Category.all
+        render json: @category
+    end
+end
--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do 
+      get "products" => "products#index"
+      get "category" => "category#index"
+    end
+  end
 end
